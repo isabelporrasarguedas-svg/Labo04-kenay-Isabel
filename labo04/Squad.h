a30d1521a6a2decb498819e2260b3032aa0aeefa
Isabel Patricia Porras Arguedas <isabel.porras.arguedas@est.una.ac.cr>
2026-03-25 15:17:17 -0600

    update Squad.h

--- labo04/Squad.h
+++ labo04/Squad.h
@@ -1,5 +1,40 @@
-#pragma once
-class Squad
-{
+#ifndef SQUAD_H
+#define SQUAD_H
+
+#include <string>
+#include "Warrior.h"
+using namespace std;
+
+class Squad {
+private:
+    string name;
+    Warrior** warriors; 
+    int capacity;
+    int count;
+
+    
+    void expand();
+
+public:
+   
+    Squad(string name, int capacity);
+
+    
+    ~Squad();
+
+    
+    Warrior* recruit(string name, string cls, int atk, int def, int hp, int maxSkills);
+    bool dismiss(string name);
+    float totalPower() const;
+    void showSquad() const;
+
+    int getCount() const;
+    string getName() const;
+
+  
+    Warrior** getByClass(string cls, int& resultCount) const;
+    void sortByPower();
+    string simulateBattle(const Squad& enemy) const;
 };
 
+#endif
