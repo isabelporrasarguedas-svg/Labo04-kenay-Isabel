7aad3cf59d5e01501cc72d46a596911d7226e110
Isabel Patricia Porras Arguedas <isabel.porras.arguedas@est.una.ac.cr>
2026-03-25 15:28:34 -0600

    update squad.cpp

--- labo04/Squad.cpp
+++ labo04/Squad.cpp
@@ -1,1 +1,44 @@
 #include "Squad.h"
+
+Warrior* Squad::recruit(string name, string cls, int atk, int def, int hp, int maxSkills)
+{
+	return nullptr;
+}
+
+bool Squad::dismiss(string name)
+{
+	return false;
+}
+
+float Squad::totalPower() const
+{
+	return 0.0f;
+}
+
+void Squad::showSquad() const
+{
+}
+
+int Squad::getCount() const
+{
+	return 0;
+}
+
+string Squad::getName() const
+{
+	return string();
+}
+
+Warrior** Squad::getByClass(string cls, int& resultCount) const
+{
+	return nullptr;
+}
+
+void Squad::sortByPower()
+{
+}
+
+string Squad::simulateBattle(const Squad& enemy) const
+{
+	return string();
+}
